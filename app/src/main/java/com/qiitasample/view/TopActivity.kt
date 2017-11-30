712833c72c18faf091f99eb02d5190c084006c32
Kosuke <kosuke.takahashi@kddi-cf.com>
2017-11-30 14:48:12 +0900

    Refactor Change Directory

--- app/src/main/java/com/qiitasample/view/TopActivity.kt
+++ app/src/main/java/com/qiitasample/view/TopActivity.kt
@@ -1,7 +1,8 @@
-package com.qiitasample
+package com.qiitasample.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.qiitasample.R
 
 class TopActivity : AppCompatActivity() {
 
